d0c0e76ca446c5c2aa66db41ae02005b5eea71ce
Jose Pedro Pereira dos Santos <josepedro.pereiradossantos@ugent.be>
2022-01-03 10:34:06 +0100

    server.go modification

--- cmd/controller/app/server.go
+++ cmd/controller/app/server.go
@@ -79,7 +79,7 @@ func Run(s *ServerRunOptions) error {
 	pgCtrl := controller.NewPodGroupController(kubeClient, pgInformer, podInformer, schedClient)
 	eqCtrl := controller.NewElasticQuotaController(kubeClient, eqInformer, podInformer, schedClient)
 	agCtrl := controller.NewAppGroupController(kubeClient, agInformer, podInformer, nodeInformer, schedClient)
-	ntCtrl := controller.NewNetworkTopologyController(kubeClient, ntInformer, nodeInformer, configmapInformer, schedClient)
+	ntCtrl := controller.NewNetworkTopologyController(kubeClient, ntInformer, agInformer, nodeInformer, podInformer, configmapInformer, schedClient)
 
 	run := func(ctx context.Context) {
 		go pgCtrl.Run(s.Workers, ctx.Done())
